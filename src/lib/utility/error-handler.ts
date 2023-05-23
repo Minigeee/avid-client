88e5e00bde011da7f6d9248867efa2b54f494b27
Minigee <tri.t.nguyen02@gmail.com>
2023-05-22 23:19:27 -0500

    a

--- src/lib/utility/error-handler.ts
+++ src/lib/utility/error-handler.ts
@@ -19,7 +19,7 @@ export function notifyError(error: Error, options?: { title?: string, message?: 
 
 	// Save error
 	if (options?.notify !== false) {
-		setExtra('error', error);
+		setExtra('error', error)
 		captureException(error);
 	}
 }
